ab05503104816f7d89df404e2abb79eb3b4752e7
czf <837629890@qq.com>
2024-05-07 00:15:53 +0800

    更新Redis工具类以及常量

--- src/main/java/com/example/lostandfoundbackground/constants/RedisConstants.java
+++ src/main/java/com/example/lostandfoundbackground/constants/RedisConstants.java
@@ -10,4 +10,8 @@ public class RedisConstants {
     public static final Long LOGIN_CODE_TTL = 3L;
     public static final String LOGIN_USER_KEY = "login:user:token:";
     public static final Long LOGIN_USER_TTL = 36000L;
+
+    public static final String ADMIN_VERIFICATION_STATUS = "admin:verificationStatus:phone:";
+
+    public static final String USER_VERIFICATION_STATUS = "user:verificationStatus:phone:";
 }
